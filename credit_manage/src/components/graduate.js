b0f88593f125a30a07cbe2b75d03d9bb12ecd254
YE-EUN <musiclove529@naver.com>
2021-11-17 04:41:29 +0900

    add title

--- credit_manage/src/components/graduate.js
+++ credit_manage/src/components/graduate.js
@@ -5,6 +5,7 @@ class Graduate extends Component{
     render(){
         return(
             <div class="content">
+                <h2 class='title'>졸업시뮬레이션</h2>
                 <Table></Table>
             </div>
         );
--- credit_manage/src/components/manage.js
+++ credit_manage/src/components/manage.js
@@ -5,6 +5,7 @@ class Manage extends Component{
     render(){
         return(
             <div class='content'>
+                <h2 class='title'>이수과목관리</h2>
                 <Table></Table>
             </div>
         );
